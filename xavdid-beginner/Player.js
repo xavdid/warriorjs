12c4231e8022b46d315cefc172bef61fbaad619c
David Brownman <beamneocube@gmail.com>
2016-07-19 14:06:39 -0700

    beginner level 4

--- xavdid-beginner/Player.js
+++ xavdid-beginner/Player.js
@@ -1,13 +1,40 @@
 class Player {
+  constructor () {
+    this._health = 20
+    this._healTo = 15
+    this._turn = 1
+  }
+
   playTurn (warrior) {
+    // console.log(`** Begin turn ${this._turn}`)
     if (warrior.feel().isEmpty()) {
-      if (warrior.health() < 10) {
+      if (warrior.health() < this._healTo && !this.underAttack(warrior)) {
+        // console.log('healing!')
         warrior.rest()
       } else {
+        // console.log('walking')
         warrior.walk()
       }
     } else {
+      // console.log('attacking')
       warrior.attack()
     }
+    // console.log('cleaning')
+    this.cleanup(warrior)
+  }
+
+  // this can't contain actions
+  cleanup (warrior) {
+    // console.log(`_health set to ${warrior.health()}`)
+    this._health = warrior.health()
+
+    // console.log(`** End turn ${this._turn}` + '\n')
+    this._turn += 1
+  }
+
+  // action checks
+  underAttack (warrior) {
+    // console.log(`_health: ${this._health} | current health: ${warrior.health()}`)
+    return this._health > warrior.health()
   }
 }
