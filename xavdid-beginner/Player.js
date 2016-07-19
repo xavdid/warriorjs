9844bbad5cc3448f1cd05bc0d447633d2a6fae5e
David Brownman <beamneocube@gmail.com>
2016-07-19 13:56:44 -0700

    beginner level 3

--- xavdid-beginner/Player.js
+++ xavdid-beginner/Player.js
@@ -1,7 +1,11 @@
 class Player {
   playTurn (warrior) {
     if (warrior.feel().isEmpty()) {
-      warrior.walk()
+      if (warrior.health() < 10) {
+        warrior.rest()
+      } else {
+        warrior.walk()
+      }
     } else {
       warrior.attack()
     }
