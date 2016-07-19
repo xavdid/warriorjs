2d2c605324a05b59fef871e670121fa7600a8aad
David Brownman <beamneocube@gmail.com>
2016-07-19 13:50:52 -0700

    beginner level 2

--- xavdid-beginner/Player.js
+++ xavdid-beginner/Player.js
@@ -1,6 +1,9 @@
 class Player {
   playTurn (warrior) {
-    // Cool code goes here
-    warrior.walk()
+    if (warrior.feel().isEmpty()) {
+      warrior.walk()
+    } else {
+      warrior.attack()
+    }
   }
 }
